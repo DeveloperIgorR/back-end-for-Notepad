a01d96315fe36fb0e08ffd1082d57d6483951577
DeveloperIgorR <bratca@inbox.ru>
2021-10-05 00:52:36 +0300

    first server fix1

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ let app = express()
 
 const port = 3000
 
+const posts = [{id:Date.now(),title: 'Hello'},
+{id:Date.now(),title: 'Hi'},
+{id:Date.now(),title: 'Il va bien'}]
+
+app.use('/posts', (req,res) => res.send(posts))
+
 app.listen(port, () => console.log('GO'))
