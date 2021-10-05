03f02acaf3634b489ab76308c57fbd5812f8ccf3
DeveloperIgorR <bratca@inbox.ru>
2021-10-06 01:10:29 +0300

    server controllers fix1

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,25 @@ const express = require('express')
 const router = express.Router()
 const UsersControllers = require('../controllers/users.controllers')
 
-router.get('/')
+router.get('/', (req, res) => {
+    try {
+        const users = UsersControllers.getUsers()
+        res.send(users)
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+})
 router.get('/:id', (req, res) => {
-    const user = UsersControllers.getUsersById(req.params.id)
-    res.send(user) 
+    try {
+        const user = UsersControllers.getUsersById(req.params.id)
+        res.send(user)
+    }
+    catch (e) {
+        console.log(e)
+    }
+
 })
 router.post('/create')
 router.put('/edit')
