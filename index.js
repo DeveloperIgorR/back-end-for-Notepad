46e0bfca10bfe97104e0469fc8db419003cc93ab
DeveloperIgorR <bratca@inbox.ru>
2021-10-07 22:39:12 +0300

    add dependeces

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const port = 3000
 
 const routes = require('./routes/index')
 app.use('/api', routes)
+app.use(express.json())
 
-app.listen(port, () => console.log('GO'))
+app.listen(port, () => console.log('Serwer Work'))
