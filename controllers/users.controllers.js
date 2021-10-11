3c9eec3517e99d6e0db4f0f5958fb8b21ed86430
DeveloperIgorR <bratca@inbox.ru>
2021-10-11 23:42:26 +0300

    authentication add

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -39,7 +39,7 @@ class UsersControllers{
             console.log(compareUser)           
             const {id} = user.id            
             if(compareUser) {
-                const token = await jwt.sign(id, process.env.ACCESS_TOKEN_SECRET)
+                const token = await jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET)
                  console.log(token)
                 return token
                
