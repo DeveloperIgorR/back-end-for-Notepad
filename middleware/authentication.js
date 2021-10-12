c04ab615becf6b2db91b393890a209c389d901d9
DeveloperIgorR <bratca@inbox.ru>
2021-10-12 23:19:57 +0300

    auth add fix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,6 +1,7 @@
 const authenticationToken = (req, res, next) => {
     try {
-        
+        const authHeader = req.headers.authorization
+        const token = authHeader && authHeader.split('')[1]
     }
      catch (e) {
         res.sendStatus(403)
