eb95cfc9debe1583f7b7ab982960054c80282b7a
DeveloperIgorR <bratca@inbox.ru>
2021-10-13 01:00:04 +0300

    add env

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,7 +1,20 @@
+const jwt = require('jsonwebtoken')
+require('dotenv/config')
+
 const authenticationToken = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization
         const token = authHeader && authHeader.split('')[1]
+        if(token == null){
+            res.sendStatus(401)
+        }
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if(err){
+                throw new Error('invalid token')
+            }
+            req.user = user
+            next()
+        })
     }
      catch (e) {
         res.sendStatus(403)
